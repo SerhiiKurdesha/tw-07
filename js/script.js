5319378e55eb3ea9f86acbf6ff9663f4e0593acc
SerhiiKurdesha <bellini1988@gmail.com>
2022-01-30 17:14:26 +0200

    sass/sass

--- js/script.js
+++ js/script.js
@@ -4,11 +4,27 @@ $(document).ready(function(){
         dots: true,
         adaptiveHeight: true,
         slidesToShow: 4,
-        slidesToScroll: 2,
+        slidesToScroll: 4,
         speed: 1000,
         infinite: true, 
         initialSlide: 1,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 500,
+        responsive:[
+            {
+                breakpoint: 1339,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
